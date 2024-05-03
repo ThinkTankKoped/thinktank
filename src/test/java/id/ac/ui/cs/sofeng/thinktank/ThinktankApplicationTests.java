269a845bd651c19fd2584a72ca23ec886e786f8f
ardhika23 <ardhika@gmail.com>
2024-05-03 19:03:38 +0700

    Initial commit

---
+++ src/test/java/id/ac/ui/cs/sofeng/thinktank/ThinktankApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.sofeng.thinktank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinktankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
